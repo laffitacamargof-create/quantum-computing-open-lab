de9958405cdba593b88b4f1a4b0d4c8dc2974d91
laffitacamargof-create <laffitacamargof@gmail.com>
2026-08-13 15:27:07 -0400

    Add files via upload

---
+++ supabase-config.js.js
@@ -0,0 +1,14 @@
+// ============================================================
+// supabase-config.js - CONFIGURACIÓN DE SUPABASE
+// ============================================================
+
+export const SUPABASE_CONFIG = {
+    // URL de tu proyecto
+    supabaseUrl: 'https://mpejkdzuofwsbdjitonr.supabase.co',
+    
+    // ✅ Clave pública (publishable)
+    supabaseAnonKey: 'sb_publishable_aDCI79qJNnOcMXd9fSXBbg_rYAKYV9d',
+    
+    // ✅ Clave secreta (secret)
+    supabaseServiceKey: 'sb_secret_qiLytp2z27jWKjUxz0pMrg__U5ZdCAu'
+};
