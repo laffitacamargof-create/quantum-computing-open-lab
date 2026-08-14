a6b5198b596f736e70b0236ec5d2503ade6e08cf
laffitacamargof-create <laffitacamargof@gmail.com>
2026-08-14 00:38:59 -0400

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -3,12 +3,12 @@
 // ============================================================
 
 export const SUPABASE_CONFIG = {
-    // URL de tu proyecto
-    supabaseUrl: 'https://mpejkdzuofwsbdjitonr.supabase.co',
+    // URL de tu nuevo proyecto
+    supabaseUrl: 'https://rtkcfbnoqbivylejhspv.supabase.co',
     
-    // ✅ Clave anónima LEGACY (para el navegador)
-    supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1wZWprZHp1b2Z3c2Jkaml0b25yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQzMjYzOTAsImV4cCI6MjA1OTkwMjM5MH0.-QbKkToS1Q-9-m6Kmm_MBvxH9p7ZxXwqV6lWY6yU1YY',
+    // ✅ Clave pública (publishable)
+    supabaseAnonKey: 'sb_publishable_Uw4ViBXo3UvvlSHzOiAcDA_9Jn6eMcf',
     
-    // ✅ Clave service_role LEGACY (para operaciones admin)
-    supabaseServiceKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1wZWprZHp1b2Z3c2Jkaml0b25yIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NDMyNjM5MCwiZXhwIjoyMDU5OTAyMzkwfQ.lM4BAB8wJkLq1A2B3C4D5E6F7G8H9I0J1K2L3M4N5O6P7Q'
+    // ✅ Clave secreta (secret)
+    supabaseServiceKey: 'sb_secret_WoC--GJvETa7jx7eGcpkQA_i0CPk1qU'
 };
